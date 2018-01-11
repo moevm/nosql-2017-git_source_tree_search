1e20739c183766d8c916b9f044a093d4cf022872
Aleksandr Ploskov <merhalak@gmail.com>
2018-01-11 18:24:16 +0300

    #11 add sub-commands {init, status, predict}
    Signed-off-by: Aleksandr Ploskov <merhalak@gmail.com>

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
+#!/usr/bin/env python3
 import argparse
+import os
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(allow_abbrev=False)
+    parser = argparse.ArgumentParser(prog='Git Source Tree Search',
+                                     allow_abbrev=False)
 
     parser.add_argument('-v', '--verbose',
                         action='store_true',
                         help='show debug messages')
 
-    parser.parse_args()
+    subparsers = parser.add_subparsers(help='Sub-command help')
+
+    init = subparsers.add_parser('init',
+                                 help='create a new project node in the Neo4j database')
+    init.set_defaults(command='init')
+    init.add_argument('project_path',
+                      action='store_const',
+                      help='path to project',
+                      metavar='PROJECT_PATH',
+                      const=os.getcwd())
+
+    status = subparsers.add_parser('status',
+                                   help='show current project status')
+    status.set_defaults(command='status')
+
+    predict = subparsers.add_parser('predict',
+                                    help='predict commit-affected unittests')
+    predict.set_defaults(command='predict')
+
+    args = vars(parser.parse_args())
+
+    if 'command' not in args:
+        parser.print_help()
+
+    return args
 
 
 if __name__ == '__main__':
-    parse_arguments()
+    args = parse_arguments()
+    print(args)
